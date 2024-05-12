ed2c9c2739f2d7e8753f0107bcefe4c12d16d37e
naobregon27 <na.obregon27@hotmail.com>
2024-05-12 16:09:43 -0300

    Pre terminado

--- landing/src/components/view3/view3.jsx
+++ landing/src/components/view3/view3.jsx
@@ -15,6 +15,11 @@ function ViewTres() {
         '/view3/Embear_Mesa de trabajo 1.png',
         '/view3/Goblina_Mesa de trabajo 1.png',
         '/view3/Perro_Mesa de trabajo 1.png',
+        "/view3/Fairan_Mesa de trabajo 1 (1).png",
+        '/view3/Foxy_Mesa de trabajo 1 (1).png',
+        '/view3/Gato_Mesa de trabajo 1 (1).png',
+        '/view3/Troll_Mesa de trabajo 1 (1).png'
+        
 
         // ... más imágenes
     ];
@@ -24,9 +29,9 @@ function ViewTres() {
         <div class="sol d-flex justify-content-center align-items-center">
         <img src={diez} class="img-fluid diez" />
         <img src={discoveri} class="img-fluid discoveri" />
+        <img src={become} class="img-fluid become" />
             <div className="carousel-container">
-                <ImageCarousel images={images} />
-                <ImageCarousel images={become} />
+                <ImageCarousel images={images} />  
             </div>
         </div>
     )
